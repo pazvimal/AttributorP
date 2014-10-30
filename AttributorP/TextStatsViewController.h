5efd8602d2660e4a3a550bb3ac54e83b3c54d9e2
Paz Patel <paz.vimal@gmail.com>
2014-10-29 22:07:48 -0400

    added blank 2nd view controller

---
+++ AttributorP/TextStatsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TextStatsViewController.h
+//  AttributorP
+//
+//  Created by Paz Patel on 10/29/14.
+//  Copyright (c) 2014 Paz Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextStatsViewController : UIViewController
+
+@end
---
+++ AttributorP/TextStatsViewController.m
@@ -0,0 +1,19 @@
+//
+//  TextStatsViewController.m
+//  AttributorP
+//
+//  Created by Paz Patel on 10/29/14.
+//  Copyright (c) 2014 Paz Patel. All rights reserved.
+//
+
+#import "TextStatsViewController.h"
+
+@interface TextStatsViewController ()
+
+@end
+
+@implementation TextStatsViewController
+
+
+
+@end
